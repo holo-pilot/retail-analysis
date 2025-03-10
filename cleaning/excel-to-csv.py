4eb1808bd34fe87800839de3055e6e6787632377
holo-pilot <hans.opena@outlook.com>
2025-03-10 14:39:34 +0000

    Updated excel-to-csv file and creted csv file

---
+++ cleaning/excel-to-csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Read the xlsx file 
+
+df = pd.read_excel('Online Retail.xlsx', sheet_name='Online Retail',header=0)
+
+# Write the DataFrame to a CSV file with the to_csv() function
+
+df.to_csv('online-retail.csv',index=False)
